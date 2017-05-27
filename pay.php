a053cdb8b7e7eb35cf1e084fb0310faf923f3ece
Christian Schaefer <schaepp@gmx.de>
2017-05-27 14:12:53 +0200

    * Heroku test

--- pay.php
+++ pay.php
@@ -2,11 +2,13 @@
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
-echo 'HALLO'; exit;
-
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 \Stripe\Stripe::setApiKey("sk_test_4a88DnIYqkODZPXlEwd4riSV");
+
+if (!isset($_POST['amount'])) {
+  exit;
+}
 
 $charge = \Stripe\Charge::create(array(
   "amount" => $_POST['amount'],
