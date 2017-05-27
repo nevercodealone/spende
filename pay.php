84e5cb68b530d6bdbabe4f0bc858fcd59b032b98
Christian Schaefer <schaepp@gmx.de>
2017-05-27 14:11:50 +0200

    * Heroku test

--- pay.php
+++ pay.php
@@ -2,6 +2,8 @@
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
+echo 'HALLO'; exit;
+
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 \Stripe\Stripe::setApiKey("sk_test_4a88DnIYqkODZPXlEwd4riSV");
